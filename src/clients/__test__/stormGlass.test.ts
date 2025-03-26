d971aa367731d057064f3c325186339e83906a3b
Eric Omine <eric.omine@gmail.com>
2025-03-25 23:23:30 -0300

    C02P01-3

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -6,13 +6,14 @@ import stormGlassWeather3HoursNormalizedFixture from '@test/fixtures/stormglass_
 jest.mock('axios');
 
 describe('StormGlass client', () => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
   it('should return the normalized forecast from the StormGlass service', async () => {
     const lat = 23.5324539;
     const long = -45.2175029;
 
-    axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixture);
+    mockedAxios.get.mockResolvedValue({ data: stormGlassWeather3HoursFixture });
 
-    const stormGlass = new StormGlass(axios);
+    const stormGlass = new StormGlass(mockedAxios);
     const response = await stormGlass.fetchPoints(lat, long);
 
     expect(response).toEqual(stormGlassWeather3HoursNormalizedFixture);
--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,14 +1,82 @@
 import { AxiosStatic } from 'axios';
 
+export interface StormGlassPointSource {
+  [key: string]: number;
+}
+
+export interface StormGlassPoint {
+  readonly time: string;
+  readonly waveDirection: StormGlassPointSource;
+  readonly swellDirection: StormGlassPointSource;
+  readonly swellHeight: StormGlassPointSource;
+  readonly swellPeriod: StormGlassPointSource;
+  readonly waveHeight: StormGlassPointSource;
+  readonly windDirection: StormGlassPointSource;
+  readonly windSpeed: StormGlassPointSource;
+}
+
+export interface StormGlassForecastResponse {
+  hours: StormGlassPoint[];
+}
+
+export interface ForecastPoint {
+  time: string;
+  waveHeight: number;
+  waveDirection: number;
+  swellDirection: number;
+  swellHeight: number;
+  swellPeriod: number;
+  windDirection: number;
+  windSpeed: number;
+}
+
 export class StormGlass {
   readonly stormGlassApiParams =
     'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,waveDirection,windSpeed';
   readonly stormGlassApiSource = 'noaa';
   constructor(protected request: AxiosStatic) {}
 
-  public async fetchPoints(lat: number, lng: number): Promise<{}> {
-    return this.request.get(
-      `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassApiParams}&source=${this.stormGlassApiSource}}`
+  public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
+    const response = await this.request.get<StormGlassForecastResponse>(
+      `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassApiParams}&source=${this.stormGlassApiSource}}`,
+      {
+        headers: {
+          Authorization: 'fake-token',
+        },
+      }
+    );
+
+    console.log(response.data.hours);
+
+    const result = this.normalizeResponse(response.data);
+    return result;
+  }
+
+  private normalizeResponse(
+    points: StormGlassForecastResponse
+  ): ForecastPoint[] {
+    return points.hours.filter(this.isValidPoint.bind(this)).map((point) => ({
+      swellDirection: point.swellDirection[this.stormGlassApiSource],
+      swellHeight: point.swellHeight[this.stormGlassApiSource],
+      swellPeriod: point.swellPeriod[this.stormGlassApiSource],
+      time: point.time,
+      waveDirection: point.waveDirection[this.stormGlassApiSource],
+      waveHeight: point.waveHeight[this.stormGlassApiSource],
+      windDirection: point.windDirection[this.stormGlassApiSource],
+      windSpeed: point.windSpeed[this.stormGlassApiSource],
+    }));
+  }
+
+  private isValidPoint(point: Partial<StormGlassPoint>): boolean {
+    return !!(
+      point.time &&
+      point.swellDirection?.[this.stormGlassApiSource] &&
+      point.swellHeight?.[this.stormGlassApiSource] &&
+      point.swellPeriod?.[this.stormGlassApiSource] &&
+      point.waveDirection?.[this.stormGlassApiSource] &&
+      point.waveHeight?.[this.stormGlassApiSource] &&
+      point.windDirection?.[this.stormGlassApiSource] &&
+      point.windSpeed?.[this.stormGlassApiSource]
     );
   }
 }
